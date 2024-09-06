e28908336d0aead8ba283872c72cb98d489834d2
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-06 18:19:44 -0400

    Add BNumber::blend_with

--- sappho-lib/src/math/bnum/bnum_math.rs
+++ sappho-lib/src/math/bnum/bnum_math.rs
@@ -1,6 +1,12 @@
 use std::ops::{Add, Sub};
 use crate::math::bnum::BNumber;
 
+impl BNumber {
+    pub fn blend_with(self, other: Self, pos: f32) -> BNumber {
+        BNumber::bound(self.val * (1f32 - pos) + other.val * pos)
+    }
+}
+
 impl Add for BNumber {
     type Output = Self;
 
