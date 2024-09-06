77c0d2d7a0d042cc26486c87dc01721e4feacf3f
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-06 18:28:14 -0400

    Add BNumber::blend_with docs and assertion

--- sappho-lib/src/math/bnum/bnum_math.rs
+++ sappho-lib/src/math/bnum/bnum_math.rs
@@ -2,7 +2,10 @@ use std::ops::{Add, Sub};
 use crate::math::bnum::BNumber;
 
 impl BNumber {
+    /// Blends the bounded values of `self` and `other` based on weight `pos`.
+    /// A `pos` of 0.0 returns `self` and a `pos` of 1.0 returns `other`
     pub fn blend_with(self, other: Self, pos: f32) -> BNumber {
+        assert!(1f32 >= pos && pos >= -1f32);
         BNumber::bound(self.val * (1f32 - pos) + other.val * pos)
     }
 }
