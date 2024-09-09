8cd01569abc7ae49dea56a4f6839e56c471d5cda
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-09 15:17:05 -0400

    Add BNumber function to create from bounded number

--- sappho-lib/src/models/bnum.rs
+++ sappho-lib/src/models/bnum.rs
@@ -10,6 +10,12 @@ pub struct BNumber {
 }
 
 impl BNumber {
+    /// Initializes a new `BNumber` from bound number `n`.
+    pub fn new(n: f32) -> BNumber {
+        assert!(1f32 > n && n > -1f32);
+        BNumber { val: n, src: BNumber::unbind(n) }
+    }
+    
     /// Initializes a new `BNumber` from unbounded number `n`.
     pub fn bound(n: f32) -> BNumber {
         BNumber { val: n / (crate::models::B_MULTI + n.abs()), src: n }
