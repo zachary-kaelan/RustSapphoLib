c8906b97248b386a711dd8bd0be3b616a7802b4c
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-10 13:27:08 -0400

    Implement Display for Actor

---
+++ sappho-lib/src/comps/actor/actor_traits.rs
@@ -0,0 +1,9 @@
+use std::fmt;
+use std::fmt::Formatter;
+use super::Actor;
+
+impl fmt::Display for Actor {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        write!(f, "{}: {}", self.id, self.personality)
+    }
+}
