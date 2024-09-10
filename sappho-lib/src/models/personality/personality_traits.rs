bcb837818f783f26e7931cb32b34fd538eec920a
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-10 13:26:41 -0400

    Fix typo

--- sappho-lib/src/models/personality/personality_traits.rs
+++ sappho-lib/src/models/personality/personality_traits.rs
@@ -38,7 +38,7 @@ mod tests {
     fn personality_display() {
         let personality = personality!(0.5f32, -0.25f32, -0.75f32, 0.0f32);
         assert_eq!(personality.to_string(),
-                   "bad_good: +0.500, false_honest: -0.250, \
+                   "bad_good: +0.500, faithless_honest: -0.250, \
                    timid_dominant: -0.750, ascetic_hedonistic: +0.000");
     }
 }
