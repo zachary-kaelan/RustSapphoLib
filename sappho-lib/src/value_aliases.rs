93e65a84b56147e0d165726f424007ce93ad0b42
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-10 15:05:26 -0400

    Rename a few aliases

--- sappho-lib/src/value_aliases.rs
+++ sappho-lib/src/value_aliases.rs
@@ -1,6 +1,6 @@
 use crate::consts::BNUM_GROUP_SIZE;
 
 pub const PERSONALITY_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["bad_good", "faithless_honest", "timid_dominant", "ascetic_hedonistic"];
-pub const PERCEPTION_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["hate_love", "mistrust_faith", "disregard_respect", "disinterest_indulgence"];
+pub const PERCEPTION_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["dislike_affection", "mistrust_faith", "disregard_heed", "respect_disgust"];
 pub const ACCORDANCE_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["misanthropy_altruism", "paranoia_gullibility", "helplessness_control", "austerity_decadence"];
 pub const VERB_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["selfish_altruistic", "deceptive_sincere", "submissive_dominant", "cold_emotional"];
