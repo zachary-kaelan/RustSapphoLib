9a93bd056cc69a8db3b1f018996b5c02af177239
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-10 17:56:18 -0400

    Change Stage to use two Vecs

--- sappho-lib/src/comps/stage.rs
+++ sappho-lib/src/comps/stage.rs
@@ -1,38 +1,51 @@
 use std::collections::HashMap;
+use std::ops::{Deref, Index};
 use std::rc::Rc;
 use crate::comps::Actor;
+use serde::{self, Deserialize, Serialize};
 
 /// A location for Actors to interact.
+#[derive(Deserialize, Serialize)]
 pub struct Stage {
     pub id: String,
     pub display_name: String,
-    actors: HashMap<String, Rc<Actor>>
+    actor_names: Vec<String>,
+    #[serde(skip)]
+    actors: Vec<Rc<Actor>>
 }
 
-impl Stage {    
-    pub fn new(id: String, display_name: String, actors: Option<HashMap<String, Rc<Actor>>>) -> Self {
-        Self { id, display_name, actors: actors.unwrap_or(HashMap::new()) }
+impl Stage {
+    pub fn new(id: String, display_name: String, actors: Option<Vec<Rc<Actor>>>) -> Self {
+        let (actor_names, actors) = match actors {
+            None => { (Vec::new(), Vec::new()) }
+            Some(a) => { (a.iter().map(|x| x.as_ref().id.clone()).collect::<Vec<_>>(), a) }
+        };
+        Self { id, display_name, actor_names, actors }
     }
 
     /// Add an actor to the stage.
     pub fn add_actor(&mut self, actor: Rc<Actor>) {
-        self.actors.insert(actor.as_ref().id.to_string(), actor);
+        self.actor_names.push(actor.as_ref().id.clone());
+        self.actors.push(actor);
     }
 
     /// Move an actor to another stage.
     pub fn move_actor_to(&mut self, actor_id: &String, other: &mut Self) {
-        other.add_actor(self.actors.remove(actor_id).unwrap())
+        let index = self.actor_names.iter().position(|x| x.eq(actor_id)).unwrap();
+        self.actor_names.remove(index);
+        other.add_actor(self.actors.remove(index))
     }
-    
+
     /// Move all actors to another stage.
     pub fn move_all_actors_to(&mut self, other: &mut Self) {
-        for actor_id in self.actors.keys() {
-            self.move_actor_to(actor_id, other)
+        for _ in 0..self.actor_names.len() {
+            other.add_actor(self.actors.pop().unwrap())
         }
+        self.actor_names.clear()
     }
-    
+
     /// Whether an actor is on a stage.
     pub fn on_stage(&self, actor_id: &String) -> bool {
-        self.actors.contains_key(actor_id)
+        todo!()
     }
 }
