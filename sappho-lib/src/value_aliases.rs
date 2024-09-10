9ca643e211ec65e73d2bb3705b68f5b28e5513cf
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-10 15:17:10 -0400

    Swap words in control_helplessness scale

--- sappho-lib/src/value_aliases.rs
+++ sappho-lib/src/value_aliases.rs
@@ -2,5 +2,5 @@ use crate::consts::BNUM_GROUP_SIZE;
 
 pub const PERSONALITY_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["bad_good", "faithless_honest", "timid_dominant", "ascetic_hedonistic"];
 pub const PERCEPTION_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["dislike_affection", "mistrust_faith", "disregard_heed", "respect_disgust"];
-pub const ACCORDANCE_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["misanthropy_altruism", "paranoia_gullibility", "helplessness_control", "austerity_decadence"];
+pub const ACCORDANCE_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["misanthropy_altruism", "paranoia_gullibility", "control_helplessness", "austerity_decadence"];
 pub const VERB_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = ["selfish_altruistic", "deceptive_sincere", "submissive_dominant", "cold_emotional"];
