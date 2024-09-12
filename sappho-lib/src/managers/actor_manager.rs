b22587bd7c0ab5b16cc692710a07f90026d80e77
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-12 14:07:30 -0400

    Add Manager function to add new actors

--- sappho-lib/src/managers/actor_manager.rs
+++ sappho-lib/src/managers/actor_manager.rs
@@ -8,8 +8,13 @@ static ACTORS: Lazy<RwLock<HashMap<String, Arc<Actor>>>> = Lazy::new(|| { RwLock
 
 impl Manager {
     pub fn get_actor(id: &String) -> std::sync::Weak<Actor> {
-        let actors = ACTORS.read().unwrap();
-        Arc::downgrade(&Arc::clone(actors.get(id).unwrap()))
+        let actors_map = ACTORS.read().unwrap();
+        Arc::downgrade(&Arc::clone(actors_map.get(id).unwrap()))
+    }
+    
+    pub fn add_actor(actor: &Arc<Actor>) {
+        let mut actors = ACTORS.write().unwrap();
+        actors.insert(actor.id.clone(), Arc::clone(actor));
     }
 
     pub fn init_actors(actors: Vec<Actor>) {
