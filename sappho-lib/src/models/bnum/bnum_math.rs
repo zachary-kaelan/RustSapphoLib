e420063abb86d77cb8dbc4e34e37c0343f6e17ea
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-13 12:22:01 -0400

    Add binary ops for BNumbers and SparseBNumbers

--- sappho-lib/src/models/bnum/bnum_math.rs
+++ sappho-lib/src/models/bnum/bnum_math.rs
@@ -1,4 +1,4 @@
-use crate::BNumber;
+use crate::{BNumber, SparseBNumber};
 use std::cmp::{Ordering, PartialEq, PartialOrd};
 use std::ops::{Add, Mul, Neg, Sub};
 
@@ -8,6 +8,18 @@ impl BNumber {
     pub fn blend_with(self, other: Self, pos: f32) -> Self {
         assert!((-1f32..=1f32).contains(&pos));
         Self::new(self.val * (1f32 - pos) + other.val * pos)
+    }
+    
+    /// Blends the bounded values of `self` and `other` based on weight `pos`.
+    /// e.g. a `pos` of 0.0 returns `self` and a `pos` of 1.0 returns `other`
+    pub fn blend_with_sparse(self, other: SparseBNumber, pos: f32) -> Self {
+        assert!((-1f32..=1f32).contains(&pos));
+        let other_value: Option<f32> = Option::from(other);
+        match other_value {
+            None => { self }
+            Some(other_value) => { Self::new(self.val * (1f32 - pos) + other_value * pos) }
+        }
+        
     }
 }
 
@@ -19,11 +31,33 @@ impl Add for BNumber {
     }
 }
 
+impl Add<SparseBNumber> for BNumber {
+    type Output = Self;
+
+    fn add(self, rhs: SparseBNumber) -> Self::Output {
+        match rhs.get_unbounded() {
+            None => { self }
+            Some(rhs_src) => { Self::bound(self.src + rhs_src) }
+        }
+    }
+}
+
 impl Sub for BNumber {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
         Self::bound(self.src - rhs.src)
+    }
+}
+
+impl Sub<SparseBNumber> for BNumber {
+    type Output = Self;
+
+    fn sub(self, rhs: SparseBNumber) -> Self::Output {
+        match rhs.get_unbounded() {
+            None => { self }
+            Some(rhs_src) => { Self::bound(self.src - rhs_src) }
+        }
     }
 }
 
--- sappho-lib/src/models/bnum_grp/bnum_grp_math.rs
+++ sappho-lib/src/models/bnum_grp/bnum_grp_math.rs
@@ -1,5 +1,5 @@
-use crate::consts::BNUM_GROUP_SIZE;
-use crate::{BNumber, BnumGroup, SparseBNumber};
+use crate::consts::{SparseBnumGroupT, BNUM_GROUP_SIZE};
+use crate::{BNumber, BnumGroup, SparseBNumber, SparseBnumGroup};
 use std::ops::{Add, Mul, Sub};
 
 impl BnumGroup {
@@ -15,6 +15,26 @@ impl BnumGroup {
             .try_into()
             .expect("Incorrect Length");
         Self { values: new_values }
+    }
+
+
+    /// Blends the bounded values of `self` and `other` based on weight `pos`.
+    /// e.g. a `pos` of 0.0 returns `self` and a `pos` of 1.0 returns `other`
+    pub fn blend_with_sparse(&self, other: &SparseBnumGroup, pos: f32) -> Self {
+        match other.values {
+            None => { *self }
+            Some(other_values) => {
+                let new_values = self
+                    .values
+                    .iter()
+                    .zip(other_values.iter())
+                    .map(|(value, other_value)| value.blend_with_sparse(*other_value, pos))
+                    .collect::<Vec<BNumber>>()
+                    .try_into()
+                    .expect("Incorrect Length");
+                Self { values: new_values } 
+            }
+        }
     }
 
     /// Sum the traits of a bnum_grp model to a single bounded value.
@@ -49,6 +69,27 @@ impl Add for BnumGroup {
     }
 }
 
+impl Add<SparseBnumGroup> for BnumGroup {
+    type Output = Self;
+
+    fn add(self, other: SparseBnumGroup) -> Self {
+        match other.values {
+            None => { self }
+            Some(other_values) => {
+                let new_values = self
+                    .values
+                    .iter()
+                    .zip(other_values.iter())
+                    .map(|(value, other_value)| *value + *other_value)
+                    .collect::<Vec<BNumber>>()
+                    .try_into()
+                    .expect("Incorrect Length");
+                Self { values: new_values }
+            }
+        }
+    }
+}
+
 impl Sub for BnumGroup {
     type Output = Self;
 
@@ -62,6 +103,27 @@ impl Sub for BnumGroup {
             .try_into()
             .expect("Incorrect Length");
         Self { values: new_values }
+    }
+}
+
+impl Sub<SparseBnumGroup> for BnumGroup {
+    type Output = Self;
+
+    fn sub(self, other: SparseBnumGroup) -> Self {
+        match other.values {
+            None => { self }
+            Some(other_values) => {
+                let new_values = self
+                    .values
+                    .iter()
+                    .zip(other_values.iter())
+                    .map(|(value, other_value)| *value - *other_value)
+                    .collect::<Vec<BNumber>>()
+                    .try_into()
+                    .expect("Incorrect Length");
+                Self { values: new_values }
+            }
+        }
     }
 }
 
