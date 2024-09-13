e618a703f6f3b6d047e4fcbe2af00fd26e4a3ef6
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-13 17:30:25 -0400

    Start VerbDef implementation

--- sappho-lib/src/comps/verb/verbdef.rs
+++ sappho-lib/src/comps/verb/verbdef.rs
@@ -1,13 +1,28 @@
-use std::collections::HashMap;
-use crate::BNumber;
 use crate::consts::BNUM_GROUP_SIZE;
+use crate::BNumber;
+use serde::{self, Deserialize, Serialize};
+use std::collections::HashMap;
 
-/// Expresses a portion of an actor's perception of a target. 
+#[derive(Deserialize, Serialize)]
+pub enum VerbType {
+    Idle,
+    ExpressPerception(ExpressPerception),
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct VerbDef {
+    pub id: String,
+    pub display_name: String,
+    verb_type: VerbType,
+}
+
+#[derive(Deserialize, Serialize)]
+/// Expresses a portion of an actor's perception of a target.
 pub struct ExpressPerception {
     /// The actor's perception of the target's bnum_grp.
     personality: Option<[Option<BNumber>; BNUM_GROUP_SIZE]>,
     /// The actor's perception of the target's emotional state.
     emotions: Option<[Option<BNumber>; BNUM_GROUP_SIZE]>,
     /// How many of each verb the actor has perceived from the target.
-    observed_verbs: Option<HashMap<String, u32>>
+    observed_verbs: Option<HashMap<String, u32>>,
 }
--- sappho-lib/src/managers/def_managers.rs
+++ sappho-lib/src/managers/def_managers.rs
@@ -1,13 +1,17 @@
-use std::collections::HashMap;
-use crate::comps::EmotionDef;
+use crate::comps::{EmotionDef, VerbDef};
 use crate::managers::Manager;
 use once_cell::sync::Lazy;
+use std::collections::HashMap;
 
 static EMOTION_DEFS: Lazy<HashMap<String, EmotionDef>> = Lazy::new(HashMap::new);
-
+static VERB_DEFS: Lazy<HashMap<String, VerbDef>> = Lazy::new(HashMap::new);
 
 impl Manager {
     pub fn get_emotion_def(id: &String) -> &EmotionDef {
         EMOTION_DEFS.get(id).unwrap()
+    }
+
+    pub fn get_verb_def(id: &String) -> &VerbDef {
+        VERB_DEFS.get(id).unwrap()
     }
 }
