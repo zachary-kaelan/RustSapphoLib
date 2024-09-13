5e64a4414710e71f3c1e20e34015d055b87934aa
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-13 17:30:14 -0400

    Start scripting support

--- sappho-lib/src/scripting/ast.rs
+++ sappho-lib/src/scripting/ast.rs
@@ -1,9 +1,35 @@
+use crate::{BNumber, BnumGroup};
+
 enum MonadicVerb {
     Negate,
+    Abs
+}
+
+enum DyadicVerb {
+    Plus,
+    Minus
 }
 
 enum AstNode {
     Float(f32),
+    BNumber(BNumber),
+    BNumGroup(BnumGroup),
+    MonadicOp {
+        verb: MonadicVerb,
+        expr: Box<AstNode>
+    },
+    DyadicOp {
+        verb: DyadicVerb,
+        lhs: Box<AstNode>,
+        rhs: Box<AstNode>
+    },
+    TriadicOp {
+        verb: DyadicVerb,
+        lhs: Box<AstNode>,
+        rhs: Box<AstNode>,
+        arg: Box<AstNode>
+    },
+    Type(String),
     Ident(String),
     Str(String),
 }
