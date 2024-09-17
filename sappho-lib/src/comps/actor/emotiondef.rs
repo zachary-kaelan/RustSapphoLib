95ae30454cc559b5f3d65d3314d9a8b074bc5e79
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-17 15:36:36 -0400

    Add EmotionDef constructor

--- sappho-lib/src/comps/actor/emotiondef.rs
+++ sappho-lib/src/comps/actor/emotiondef.rs
@@ -11,6 +11,24 @@ pub struct EmotionDef {
 }
 
 impl EmotionDef {
+    pub fn new(
+        id: String,
+        display_name: String,
+        personality_modifiers: SparseBnumGroup,
+        personality_weights: Option<[Option<f32>; BNUM_GROUP_SIZE]>,
+        perception_modifiers: SparseBnumGroup,
+        perception_weights: Option<[Option<f32>; BNUM_GROUP_SIZE]>,
+    ) -> Self {
+        Self {
+            id,
+            display_name,
+            personality_modifiers,
+            personality_weights,
+            perception_modifiers,
+            perception_weights,
+        }
+    }
+
     pub fn apply_to_personality(&self, values: BnumGroup, intensity: f32) -> BnumGroup {
         let modified_values = match self.personality_weights {
             None => values,
