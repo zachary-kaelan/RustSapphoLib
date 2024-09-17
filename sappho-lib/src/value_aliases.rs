31453a62fad39bee14364e7834e6a2318b616f06
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-17 18:18:48 -0400

    Fix typo and add another helper function for getting value aliases

--- sappho-lib/src/value_aliases.rs
+++ sappho-lib/src/value_aliases.rs
@@ -1,4 +1,5 @@
 use crate::consts::BNUM_GROUP_SIZE;
+use crate::scripting::BnumType;
 
 pub enum ValueAliasType {
     Personality,
@@ -9,10 +10,20 @@ pub enum ValueAliasType {
 
 pub fn get_value_aliases_from_type(alias_type: ValueAliasType) -> [&'static str; BNUM_GROUP_SIZE] {
     match alias_type {
-        ValueAliasType::Personality => { PERCEPTION_VALUE_ALIASES }
+        ValueAliasType::Personality => { PERSONALITY_VALUE_ALIASES }
         ValueAliasType::Perception => { PERCEPTION_VALUE_ALIASES }
         ValueAliasType::Accordance => { ACCORDANCE_VALUE_ALIASES }
         ValueAliasType::Verb => { VERB_VALUE_ALIASES }
+    }
+}
+
+pub fn get_value_aliases_from_bnum_type(bnum_type: &BnumType) -> [&'static str; BNUM_GROUP_SIZE] {
+    match bnum_type {
+        BnumType::Personality => { PERSONALITY_VALUE_ALIASES }
+        BnumType::Accordance => { ACCORDANCE_VALUE_ALIASES }
+        BnumType::Perception => { PERCEPTION_VALUE_ALIASES }
+        BnumType::Circumstantial => { PERCEPTION_VALUE_ALIASES }
+        BnumType::SelfPerception => { PERCEPTION_VALUE_ALIASES }
     }
 }
 
