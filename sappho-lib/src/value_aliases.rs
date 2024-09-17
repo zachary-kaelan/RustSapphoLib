9506aad4256dc8e65b392759072ef3ab04b9bac2
zach.johnson <zach.johnson@Vaduminc.com>
2024-09-17 15:38:50 -0400

    Add helper function for getting value aliases

--- sappho-lib/src/value_aliases.rs
+++ sappho-lib/src/value_aliases.rs
@@ -1,5 +1,21 @@
 use crate::consts::BNUM_GROUP_SIZE;
 
+pub enum ValueAliasType {
+    Personality,
+    Perception,
+    Accordance,
+    Verb
+}
+
+pub fn get_value_aliases_from_type(alias_type: ValueAliasType) -> [&'static str; BNUM_GROUP_SIZE] {
+    match alias_type {
+        ValueAliasType::Personality => { PERCEPTION_VALUE_ALIASES }
+        ValueAliasType::Perception => { PERCEPTION_VALUE_ALIASES }
+        ValueAliasType::Accordance => { ACCORDANCE_VALUE_ALIASES }
+        ValueAliasType::Verb => { VERB_VALUE_ALIASES }
+    }
+}
+
 pub const PERSONALITY_VALUE_ALIASES: [&str; BNUM_GROUP_SIZE] = [
     "bad_good",
     "faithless_honest",
